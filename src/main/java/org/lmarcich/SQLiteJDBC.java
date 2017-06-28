c1ab395a12b84051e3e5006919826371c278507e
Larissa Marcich <lmarcich@thoughtworks.com>
2017-06-27 22:52:30 -0400

    Switch to in-memory db for demonstration purposes

--- src/main/java/org/lmarcich/SQLiteJDBC.java
+++ src/main/java/org/lmarcich/SQLiteJDBC.java
@@ -1,105 +1,87 @@
 package org.lmarcich;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SQLiteJDBC {
 
-    public static void main(String args[]) {
-        createTable();
-        insert();
-        select();
-    }
+    private static final String DATABASE_URL = "jdbc:sqlite::memory:";
 
-    static void createTable() {
-        try {
-            Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            System.out.println("Opened database successfully");
-
-            Statement stmt = c.createStatement();
-            String sql = "CREATE TABLE COMPANY " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " AGE            INT     NOT NULL, " +
-                    " ADDRESS        CHAR(50), " +
-                    " SALARY         REAL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
+    static void createTable(Connection c) throws SQLException {
+        Statement stmt = c.createStatement();
+        String sql = "CREATE TABLE COMPANY " +
+                "(ID INT PRIMARY KEY     NOT NULL," +
+                " NAME           TEXT    NOT NULL, " +
+                " AGE            INT     NOT NULL, " +
+                " ADDRESS        CHAR(50), " +
+                " SALARY         REAL)";
+        stmt.executeUpdate(sql);
+        stmt.close();
         System.out.println("Table created successfully");
     }
 
-    static void insert() {
-        try {
-            Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+    static void insert(Connection c) throws SQLException {
+        c.setAutoCommit(false);
+        Statement stmt = c.createStatement();
+        String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+        stmt.executeUpdate(sql);
 
-            Statement stmt = c.createStatement();
-            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                    "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
-            stmt.executeUpdate(sql);
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+        stmt.executeUpdate(sql);
 
-            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                    "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
-            stmt.executeUpdate(sql);
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+        stmt.executeUpdate(sql);
 
-            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                    "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
-            stmt.executeUpdate(sql);
+        sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+        stmt.executeUpdate(sql);
 
-            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-                    "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
-            stmt.executeUpdate(sql);
+        stmt.close();
+        c.commit();
 
-            stmt.close();
-            c.commit();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        System.out.println("Records created successfully");
+        System.out.println("Records inserted successfully");
     }
 
 
-    static void select() {
+    static void select(Connection c) throws SQLException {
+        c.setAutoCommit(false);
+        Statement stmt = c.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            String address = rs.getString("address");
+            float salary = rs.getFloat("salary");
+
+            System.out.println("ID = " + id);
+            System.out.println("NAME = " + name);
+            System.out.println("AGE = " + age);
+            System.out.println("ADDRESS = " + address);
+            System.out.println("SALARY = " + salary);
+            System.out.println();
+        }
+        rs.close();
+        stmt.close();
+
+        System.out.println("Selection performed successfully");
+    }
+
+
+    public static void main(String args[]) {
         try {
-            Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
-
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                String address = rs.getString("address");
-                float salary = rs.getFloat("salary");
-
-                System.out.println("ID = " + id);
-                System.out.println("NAME = " + name);
-                System.out.println("AGE = " + age);
-                System.out.println("ADDRESS = " + address);
-                System.out.println("SALARY = " + salary);
-                System.out.println();
-            }
-            rs.close();
-            stmt.close();
+            Connection c = DriverManager.getConnection(DATABASE_URL);
+            createTable(c);
+            insert(c);
+            select(c);
             c.close();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        System.out.println("Operation done successfully");
     }
 }
 
